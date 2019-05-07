26fd94096a10a724409ccb65f57ea70d4adf8788
Deepak Chand <deepak.chand@nagarro.com>
2019-05-07 20:54:45 +0530

    commit

--- config/secret.js
+++ config/secret.js
@@ -1,6 +1,5 @@
 module.exports = {
-	
-	database: 'mongodb://root:123@ds117830.mlab.com:17830/onepiece',
-	port: 3000,
-	secretKey: "Deepak@#$##"
-}
+  database: 'mongodb://root:123@ds117830.mlab.com:17830/onepiece',
+  port: 3000,
+  secretKey: 'Deepak@#$##'
+};
